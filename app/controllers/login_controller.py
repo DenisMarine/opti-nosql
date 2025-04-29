b7a1788064987ac67aaaa432ae688806a19c43ff
Mathieu MORGAT <m.morgat@bweez.com>
2025-04-29 15:08:36 +0200

    feat(login): implement post login

--- app/controllers/login_controller.py
+++ app/controllers/login_controller.py
@@ -1,3 +1,14 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Query, HTTPException
+from app.services.login_service import login
 
 router = APIRouter()
+
+@router.post("/login")
+async def login_user(
+  user_id: str = Query(..., alias="userId")
+):
+  # Login a user and return a token with expiry time
+  try:
+    return await login(user_id)
+  except Exception as e:
+    raise HTTPException(status_code=500, detail=str(e))
